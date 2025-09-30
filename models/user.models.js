0820329a0fc7f12de736e88b775533fd1dddd552
Dhruv <dhruvshetty2203@gmail.com>
2025-10-01 00:22:39 +0530

    user model updated with token handling

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,8 @@
 import { time } from "console";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { use } from "react";
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,7 +25,54 @@ const userSchema = new mongoose.Schema(
       select: false,
       match: /^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/,
     },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+userSchema.methods.getAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.ACESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACESS_TOKEN_EXPIRES,
+    }
+  );
+};
+
+userSchema.methods.refreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRES,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
